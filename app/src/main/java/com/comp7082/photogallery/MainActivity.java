f2321e1ec6e1d8ef53991c89b33174af4bbef8f8
keithliu03 <keithliu2013@gmail.com>
2021-09-15 21:11:13 -0700

    TakePhotoWorks

--- app/src/main/java/com/comp7082/photogallery/MainActivity.java
+++ app/src/main/java/com/comp7082/photogallery/MainActivity.java
@@ -2,13 +2,39 @@ package com.comp7082.photogallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }   catch(ActivityNotFoundException e){
+            Log.e(TAG, "Error");
+        }
     }
 }
