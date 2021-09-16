80372139ca4a9a6d969bc5e3df99d774cec9995d
keithliu03 <keithliu2013@gmail.com>
2021-09-16 01:41:32 -0700

    Committing Kiwon Branch + Changes
    Scrolling through photos is working as intended

--- app/src/main/java/com/comp7082/photogallery/MainActivity.java
+++ app/src/main/java/com/comp7082/photogallery/MainActivity.java
@@ -29,12 +29,99 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+<<<<<<< Updated upstream
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         try {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }   catch(ActivityNotFoundException e){
             Log.e(TAG, "Error");
+=======
+    public void nextPhoto(View v) {
+        if (photos.size() > 0) {
+            if (index < (photos.size() - 1)) {
+                index++;
+            }
+            Log.i(TAG, "Current file at index: " + index + " | " + photos.get(index));
+            System.out.print(index);
+            displayPhoto(photos.get(index));
+            //updatePhoto(photos.get(index), ((EditText) findViewById(R.id.etCaption)).getText().toString());
+        }
+    }
+    public void previousPhoto(View v) {
+        if (photos.size() > 0) {
+            if (index > 0) {
+                index--;
+            }
+            Log.i(TAG, "Current file at index: " + index + " | " + photos.get(index));
+            System.out.print(index);
+            displayPhoto(photos.get(index));
+            //updatePhoto(photos.get(index), ((EditText) findViewById(R.id.etCaption)).getText().toString());
+        }
+    }
+    private void displayPhoto(String path) {
+        ImageView iv = (ImageView) findViewById(R.id.ivGallery);
+        TextView tv = (TextView) findViewById(R.id.tvTimestamp);
+        EditText et = (EditText) findViewById(R.id.etCaption);
+        if (path == null || path.equals("")) {
+            iv.setImageResource(R.mipmap.ic_launcher);
+            et.setText("");
+            tv.setText("");
+        } else {
+            iv.setImageBitmap(BitmapFactory.decodeFile(path));
+            String[] attr = path.split("_");
+            et.setText(attr[1]);
+            tv.setText(attr[2]);
+        }
+    }
+    private File createImageFile() throws IOException {
+// Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "_caption_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg",storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    private void updatePhoto(String path, String caption) {
+        String[] attr = path.split("_");
+        if (attr.length >= 3) {
+            File to = new File(attr[0] + "_" + caption + "_" + attr[2] + "_" + attr[3]);
+            File from = new File(path);
+            from.renameTo(to);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SEARCH_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                DateFormat format = new SimpleDateFormat("yyyy‐MM‐dd HH:mm:ss");
+                Date startTimestamp , endTimestamp;
+                try {
+                    String from = (String) data.getStringExtra("STARTTIMESTAMP");
+                    String to = (String) data.getStringExtra("ENDTIMESTAMP");
+                    startTimestamp = format.parse(from);
+                    endTimestamp = format.parse(to);
+                } catch (Exception ex) {
+                    startTimestamp = null;
+                    endTimestamp = null;
+                }
+                String keywords = (String) data.getStringExtra("KEYWORDS");
+                index = 0;
+                photos = findPhotos(startTimestamp, endTimestamp, keywords);
+                if (photos.size() == 0) {
+                    displayPhoto(null);
+                } else {
+                    displayPhoto(photos.get(index));
+                }
+            }
+        }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            ImageView mImageView = (ImageView) findViewById(R.id.ivGallery);
+            mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
+            photos = findPhotos(new Date(Long.MIN_VALUE), new Date(), "");
+>>>>>>> Stashed changes
         }
     }
 }
